c987b83bc8f23b67d4916447bc227eed9837ef49
kataya <shinji_kataya@esrij.com>
2024-08-21 10:48:39 +0900

    add comments

--- RenameFolderService.py
+++ RenameFolderService.py
@@ -4,30 +4,33 @@ from SMWinservice import SMWinservice
 # PATH OF INTEREST:
 watched_path = r'C:\Coding\Folder Watcher Service\CSV Files'
 
+# class that inherits from SMWinservice
 class RenameFolderService(SMWinservice):
     _svc_name_ = 'RenameFolderService'
     _svc_display_name_ = 'Py Folder Observer'
     _svc_description_ = r'Observes and renames CSV files of particular folder: C:\Coding\Folder Watcher Service\CSV Files'
 
+    # Override the three main methods: 
     def start(self):
-        '''When start service is pressed'''
+        '''When start winservice is pressed'''
         self.isrunning = True
 
     def stop(self):
-        '''When Service is stopped'''
-        self.service.stop()
+        '''When winservice is stopped'''
+        self.service.stop() #call self.service.stop() method
         self.isrunning = False
         self.iswaching = False
 
     def main(self):
-        '''When service is running, do below'''
+        '''When winservice is running, do below'''
         self.iswaching = False
         # Store class with path argument as self.service
         self.service = CsvWatcher(watched_path)
         while self.isrunning:
             if not self.iswaching:
-                self.service.run()
+                self.service.run() #call self.service.run() method
             self.iswaching = True
 
+# entry point
 if __name__ == '__main__':
     RenameFolderService.parse_command_line()
--- SMWinservice.py
+++ SMWinservice.py
@@ -1,3 +1,44 @@
+# How to create a Windows Service in Python
+# https://thepythoncorner.com/posts/2018-08-01-how-to-create-a-windows-service-in-python/
+'''
+SMWinservice
+by Davide Mastromatteo
+
+Base class to create winservice in Python
+Python で winservice を作成するための基底クラス
+-----------------------------------------
+
+Instructions:
+手順:
+
+1. Just create a new class that inherits from this base class
+   この基底クラスを継承した新しいクラスを作成します
+2. Define into the new class the variables
+   新しいクラスに次の変数を定義する
+   _svc_name_ = "nameOfWinservice"
+                "Winserviceの名称"
+   _svc_display_name_ = "name of the Winservice that will be displayed in scm"
+                        "サービス コントロール マネージャー(scm)でのWinserviceの表示名"
+   _svc_description_ = "description of the Winservice that will be displayed in scm"
+                       "サービス コントロール マネージャー(scm)scmでのWinserviceの説明"
+3. Override the three main methods:
+   3つのメインメソッドをオーバーライドする
+    def start(self) : if you need to do something at the service initialization.
+                      A good idea is to put here the inizialization of the running condition
+                      サービスの初期化時に何かをする必要がある場合のメソッド
+                      実行状態の初期化をここにに置くのはいいアイディアです
+    def stop(self)  : if you need to do something just before the service is stopped.
+                      A good idea is to put here the invalidation of the running condition
+                      サービスの停止する直前に何かをする必要がある場合のメソッド
+                      実行状態の無効化をここに置いておくのがいいアイディアです
+    def main(self)  : your actual run loop. Just create a loop based on your running condition
+                      実際の実行ループ。実行条件に基づいてループを作成する。
+4. Define the entry point of your module calling the method "parse_command_line" of the new class
+   新しいクラスで"parse_command_line"のメソッドを呼び出し、モジュールのエントリーポイントを定義する
+5. Enjoy
+   楽しむ
+'''
+
 import socket
 import win32serviceutil
 import servicemanager
